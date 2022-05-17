089b02b8ef8f476c70278aec5d112a2c3ef5651a
Travis Fehr <travisefehr@gmail.com>
2022-05-16 18:41:05 -0700

    commit tree 988fceb8cc4f07ac68618e798134a8a11c5cfdf2

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your code here!
+function removeMain() {
+  let main = document.getElementById('main')
+  main.remove();
+}
+
+removeMain()
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.setAttribute('h1', 'victory');
+
+function makingAChampion(userInput) {
+  newHeader.textContent = `${userInput} is the champion`
+}
+
+makingAChampion()
